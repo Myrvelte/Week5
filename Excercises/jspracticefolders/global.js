c00bfbbd529123d6e32f4a9989d7e3b36df19ff7
Myrvelte <myrvelte@gmail.com>
2024-10-24 20:07:23 -0400

    practice

---
+++ Excercises/jspracticefolders/global.js
@@ -0,0 +1,12 @@
+let globalVar "This is a global variable";
+
+function displayGlobalVar() {
+    console.log(globalVar);
+}
+
+function boo!local scope example() {
+let localVar = "This is a local variable";//LOCAL
+console.log(localVar);
+}
+
+displayGlobalVar();
---
+++ Excercises/jspracticefolders/objects.js
@@ -0,0 +1,13 @@
+let person = {
+    name: "Myrvelte"
+    age: 30
+    isStudent: true,
+};
+
+person.city = "Paris"
+
+console.log(typeof person.name);
+console.log(typeof person.age);
+
+console.log(person["name"]);
+console.log(person.city);
